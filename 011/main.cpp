c0f1ed1ef0deff30f56919ca264decd60e5d4eaa
汪恳 <1174843091@qq.com>
2015-04-09 18:32:48 +0800

    Create main.cpp

---
+++ 011/main.cpp
@@ -0,0 +1,21 @@
+/**************************************
+ *FILE    :011.cpp
+ *PROJECT :NULL
+ *AUTHOR  :707wk
+ *CREATED :4/9/2015
+***************************************/
+/*
+using class achieve input sin
+*/
+#include <iostream>
+#include "inputsin.h"
+
+using namespace std;
+
+int main()
+{
+	inputsin temp;
+	temp.setnum();
+	temp.putnum();
+	return 0;
+}
