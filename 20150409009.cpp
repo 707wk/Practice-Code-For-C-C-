4a3eef34d3031690bbf736be88e187363beb2dc3
汪恳 <1174843091@qq.com>
2015-04-09 18:30:38 +0800

    Create 20150409009.cpp

---
+++ 20150409009.cpp
@@ -0,0 +1,51 @@
+/**************************************
+ *FILE    :009.cpp
+ *PROJECT :NULL
+ *AUTHOR  :707wk
+ *CREATED :4/9/2015
+***************************************/
+/*
+input cos
+*/
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+#define HIGHT 20
+#define WIDE 62
+
+int num[HIGHT][WIDE]={0};
+
+void setnum()
+{
+	int j;
+	int temp;
+	for(j=0;j<WIDE;j++)
+	{
+		temp=HIGHT/2-(int)((cos(j/(WIDE/62)/10.0)+0.05)*HIGHT/2);
+		num[temp][j]=1;
+	}
+}
+
+void putnum()
+{
+	int i;
+	int j;
+	for(i=0;i<HIGHT;i++)
+	{
+		for(j=0;j<WIDE;j++)
+		{
+			if(num[i][j])cout<<"*";
+			else cout<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	setnum();
+	putnum();
+	return 0;
+}
