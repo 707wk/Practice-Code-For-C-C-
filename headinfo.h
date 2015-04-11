3b63036da76791c53d185834b7faee5b4fa72888
汪恳 <1174843091@qq.com>
2015-04-11 23:44:19 +0800

    Create headinfo.h

---
+++ headinfo.h
@@ -0,0 +1,8 @@
+/**************************************
+ *FILE    :main.cpp
+ *PROJECT :NULL
+ *AUTHOR  :707wk
+ *CREATED :4/9/2015
+ *TEXT     :
+ *EMAIL    :gtsoft_wk@foxmail.com
+***************************************/
