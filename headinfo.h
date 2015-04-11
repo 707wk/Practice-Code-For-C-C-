21e604b2697fea65f508f60b2a16131d64310d2a
汪恳 <1174843091@qq.com>
2015-04-12 00:29:53 +0800

    Update headinfo.h

--- headinfo.h
+++ headinfo.h
@@ -2,7 +2,7 @@
  *FILE    :main.cpp
  *PROJECT :NULL
  *AUTHOR  :707wk
- *CREATED :4/9/2015
- *TEXT     :
- *EMAIL    :gtsoft_wk@foxmail.com
+ *CREATED :4/11/2015
+ *TEXT    :
+ *EMAIL   :gtsoft_wk@foxmail.com
 ***************************************/
